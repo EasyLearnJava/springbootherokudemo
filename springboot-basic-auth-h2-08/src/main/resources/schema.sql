5e09f7310ad75662de763f12da8d0ac496b19bca
Raghu <easylearnjava4u@gmail.com>
2019-02-06 19:37:28 -0600

    Update sql related info

--- springboot-basic-auth-h2-08/src/main/resources/schema.sql
+++ springboot-basic-auth-h2-08/src/main/resources/schema.sql
@@ -1,5 +1,7 @@
+drop table if exists customers;
+
 CREATE  TABLE customers (
-	id integer NOT Null,
+	id integer NOT Null AUTO_INCREMENT,
   username VARCHAR(45) NOT NULL ,
   role VARCHAR(45) NOT NULL ,
   emailid VARCHAR(45) NOT NULL ,
