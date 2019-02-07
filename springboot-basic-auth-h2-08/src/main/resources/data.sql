50dd9c1a02723cb5bc37331d79367d2c0dc1ca20
Raghu <easylearnjava4u@gmail.com>
2019-02-06 19:05:37 -0600

    Adding sql files for data setup

---
+++ springboot-basic-auth-h2-08/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customers(username,role, emailid,password) VALUES ('raghu','admin', 'raghu@test.com', 'dbsecret');
+
+INSERT INTO customers(username,role, emailid,password) VALUES ('ramya','oremium member', 'ramya@test.com', 'dbnewsecret');
---
+++ springboot-basic-auth-h2-08/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE  TABLE customers (
+	id integer NOT Null,
+  username VARCHAR(45) NOT NULL ,
+  role VARCHAR(45) NOT NULL ,
+  emailid VARCHAR(45) NOT NULL ,
+  password VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id));
