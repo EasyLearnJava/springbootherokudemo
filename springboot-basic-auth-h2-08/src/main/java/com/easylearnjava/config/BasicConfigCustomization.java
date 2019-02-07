8c24ad3e8cbf580237a05db659598ab04d516f90
Raghu <easylearnjava4u@gmail.com>
2019-02-07 08:05:13 -0600

    Customizing the rest cross origin

--- springboot-basic-auth-h2-08/src/main/java/com/easylearnjava/config/BasicConfigCustomization.java
+++ springboot-basic-auth-h2-08/src/main/java/com/easylearnjava/config/BasicConfigCustomization.java
@@ -1,10 +1,14 @@
 package com.easylearnjava.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -31,8 +35,15 @@ public class BasicConfigCustomization extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().
-		and().authorizeRequests().antMatchers("/h2-console/**").permitAll().
-		and().csrf().ignoringAntMatchers("/h2-console/**");
-		http.headers().frameOptions().disable();
+		and().authorizeRequests().antMatchers("/h2-console/**","/customers/**").permitAll().
+		and().csrf().ignoringAntMatchers("/h2-console/**","/customers/**");
+		http.headers().frameOptions().disable().
+		and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+     
+    /* To allow Pre-flight [OPTIONS] request from browser */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 }
