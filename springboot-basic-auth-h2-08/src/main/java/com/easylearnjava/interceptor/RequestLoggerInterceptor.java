97a6fe416a5a6234ce867dbcbd0afd9be90114ef
Raghu <easylearnjava4u@gmail.com>
2019-02-07 07:52:04 -0600

    Adding Cross origin for the Controller

--- springboot-basic-auth-h2-08/src/main/java/com/easylearnjava/interceptor/RequestLoggerInterceptor.java
+++ springboot-basic-auth-h2-08/src/main/java/com/easylearnjava/interceptor/RequestLoggerInterceptor.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component("RequestLoggerInterceptor")
@@ -23,17 +21,13 @@ public class RequestLoggerInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 		
-		LOGGER.info("********** Start of Request Info **********");
-		LOGGER.info("********** Requested URL {} ", request.getRequestURI());
-		LOGGER.info("********** Request from user agent {} ", request.getHeader("user-agent"));
-        LOGGER.info("********** Request from Proxy Ip {} ", request.getRemoteAddr());
-        LOGGER.info("********** Request from actual client IP {} : ", request.getHeader("X-Forwarded-For"));
-        
-        Map<String, String> items = getHeadersInfo(request);
-        
-        items.forEach((key,value)->System.out.println("################# Header : " + key + " value : " + value));
-        
-        LOGGER.info("########## End of Request Info ##########");
+		if(LOGGER.isDebugEnabled()) {
+			LOGGER.info("********** Start of Request Info **********");
+		
+			LOGGER.info("********** Requested URL {} ", request.getRequestURI());			
+	        
+	        LOGGER.info("########## End of Request Info ##########");
+		}
         
         return true;
     }
