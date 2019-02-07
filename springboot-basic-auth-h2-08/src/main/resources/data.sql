5df00510e47f94a2b5e738c7a047c5eb0e4ada85
Raghu <easylearnjava4u@gmail.com>
2019-02-06 20:24:43 -0600

    set correct field names

--- springboot-basic-auth-h2-08/src/main/resources/data.sql
+++ springboot-basic-auth-h2-08/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO customer(id,username,role, emailid,password) VALUES (1,'raghu','admin', 'raghu@test.com', 'dbsecret');
+INSERT INTO customer(name,role, emailid,password) VALUES ('raghu','admin', 'raghu@test.com', 'dbsecret');
 
-INSERT INTO customer(id,username,role, emailid,password) VALUES (2,'ramya','oremium member', 'ramya@test.com', 'dbnewsecret');
+INSERT INTO customer(name,role, emailid,password) VALUES ('ramya','oremium member', 'ramya@test.com', 'dbnewsecret');
