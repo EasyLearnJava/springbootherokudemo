2d12c8366625111427a782ad5d6e0d90304c8b5f
Raghu <easylearnjava4u@gmail.com>
2019-02-06 20:34:30 -0600

    Set id

--- springboot-basic-auth-h2-08/src/main/resources/data.sql
+++ springboot-basic-auth-h2-08/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO customer(name, role, email_id, password) VALUES ('raghu','admin', 'raghu@test.com', 'dbsecret');
+INSERT INTO customer(id, name, role, email_id, password) VALUES (1, 'raghu','admin', 'raghu@test.com', 'dbsecret');
 
-INSERT INTO customer(name, role, email_id, password) VALUES ('ramya','oremium member', 'ramya@test.com', 'dbnewsecret');
+INSERT INTO customer(id, name, role, email_id, password) VALUES (2, 'ramya','oremium member', 'ramya@test.com', 'dbnewsecret');
