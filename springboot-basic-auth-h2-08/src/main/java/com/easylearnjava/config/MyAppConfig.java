8fcefd5f483e38e2fcace9cdaad1be978a608cdd
Raghu <easylearnjava4u@gmail.com>
2019-02-07 07:12:47 -0600

    Registering the interceptor

---
+++ springboot-basic-auth-h2-08/src/main/java/com/easylearnjava/config/MyAppConfig.java
@@ -0,0 +1,23 @@
+package com.easylearnjava.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.easylearnjava.interceptor.RequestLoggerInterceptor;
+
+@Configuration
+public class MyAppConfig implements WebMvcConfigurer {
+	
+	@Autowired
+    @Qualifier("RequestLoggerInterceptor")
+	RequestLoggerInterceptor requestLogger;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.requestLogger);
+    }
+
+}
